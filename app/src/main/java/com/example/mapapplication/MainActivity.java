2b00c4cdec9d35fc21f172af45b1f3683c875133
TUAN2003 <tuananhvund0321@gmail.com>
2023-09-17 04:03:45 +0700

    clean icons,changed coding

--- app/src/main/java/com/example/mapapplication/MainActivity.java
+++ app/src/main/java/com/example/mapapplication/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.mapapplication;
 
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -19,13 +17,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
